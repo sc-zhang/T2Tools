97783988300f0baaec4750ef3986d9fb170946c4
sc-zhang <zsc-zhang@foxmail.com>
2024-09-11 15:30:41 +0800

    adjust order

--- t2tools/workflow/gap_cnt_pipe.py
+++ t2tools/workflow/gap_cnt_pipe.py
@@ -13,7 +13,7 @@ def main(args):
         Message.info("Writing gap counts")
         with open(output_file, 'w') as fout:
             for sid in sorted(gap_db, key=lambda x: int(re.findall(r'(\d+)', x)[0])
-                              if len(re.findall(r'(\d+)', x)) > 0 else 0):
+                              if len(re.findall(r'(\d+)', x)) > 0 else 1000):
                 if sid != "Total":
                     fout.write("%s\t%s\n" % (sid, gap_db[sid]))
             fout.write("Total\t%s\n" % gap_db["Total"])
@@ -21,7 +21,7 @@ def main(args):
     else:
         gap_db = Fasta.get_fasta_gaps(input_fasta)
         for sid in sorted(gap_db, key=lambda x: int(re.findall(r'(\d+)', x)[0])
-                          if len(re.findall(r'(\d+)', x)) > 0 else 0):
+                          if len(re.findall(r'(\d+)', x)) > 0 else 1000):
             if sid != "Total":
                 print("%s\t%s" % (sid, gap_db[sid]))
         print("Total\t%s" % gap_db["Total"])
